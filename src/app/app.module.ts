0b7e836b584dae615696abc10995b6f5ec96eefe
t-inoue <t-inoue@osaka-univ.coop>
2017-07-11 16:00:40 +0900

    #12 test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
-
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent,
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent,
-      },
-    ]),
   ],
   providers: [
     HeroService,
